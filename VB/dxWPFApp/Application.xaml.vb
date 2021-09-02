a0265050270d204afdf10399127da8bca6c67abe
DevExpressExampleBot <fakeEmail>
2021-09-02 19:34:45 +0300

    Source auto update [skip ci]

--- VB/dxWPFApp/Application.xaml.vb
+++ VB/dxWPFApp/Application.xaml.vb
@@ -7,14 +7,14 @@ Imports System.Windows
 Imports DevExpress.Xpf.Core
 
 Namespace dxWPFApp
-    ''' <summary>
-    ''' Interaction logic for App.xaml
-    ''' </summary>
-    Partial Public Class App
-        Inherits Application
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
 
-        Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
-            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
-        End Sub
-    End Class
+		Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
+			DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
+		End Sub
+	End Class
 End Namespace
